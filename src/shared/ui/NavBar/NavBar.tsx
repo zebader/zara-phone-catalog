042be1124b6a9aff88b88a2ef3e4c1ae43838218
zebader <zebader@hotmail.com>
2026-06-23 12:45:12 +0200

    fix: navbar navigation and sticky

--- src/shared/ui/NavBar/NavBar.tsx
+++ src/shared/ui/NavBar/NavBar.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import styles from './NavBar.module.css';
 import logo from '../../assets/mbst-logo.svg';
 import { NavBarCart } from './NavBarCart/NavBarCart';
-
+import Link from 'next/link';
 export const NavBar = () => {
 
   return (
     <nav className={styles.container}>
-      <div className={styles.imageWrapper}>
+      <Link href="/" className={styles.imageWrapper}>
         <Image src={logo} alt="Logo para volver a la pagina principal" width={74} height={24} />
-      </div>
+      </Link>
       <NavBarCart />
     </nav>
   );
